d640b86a8597bcbb6fe7fd749eecffe6e225bc04
William Chong <me@williamchong.cloud>
2023-03-06 15:53:42 +0800

    🚚 Move send-like to this repo

---
+++ send-like/README.md
@@ -0,0 +1,25 @@
+# send LIKE
+
+send LIKE from csv
+
+## Installation
+
+Run with `node.js >= 14`
+
+```bash
+npm install
+```
+
+## Configuration
+
+Change the data in `./list.csv` (reference `./list_example.csv`)
+
+Change the data in `./index.js`
+
+Where `mnemonic` is your mnemonic phrase , `memo` is the memo you want, `waitTime` is the delay before actual transaction after console prints total
+
+## Usage
+
+```bash
+node index.js
+```
---
+++ send-like/index.js
@@ -0,0 +1,77 @@
+import { DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
+import { SigningStargateClient } from "@cosmjs/stargate";
+import { MsgSend } from 'cosmjs-types/cosmos/bank/v1beta1/tx.js';
+import fs from 'fs';
+import neatCsv from 'neat-csv';
+import BigNumber from 'bignumber.js';
+
+////////////// use your mnemonic phrase //////////////
+const mnemonic = process.env.MNEMONIC;
+////////////// use your desired memo //////////////
+const memo = process.env.MEMO || '';
+////////////// adjust delay //////////////
+const waitTime = Number(10000)
+
+const wallet = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic, { prefix: 'like' });
+const [firstAccount] = await wallet.getAccounts();
+const rpcEndpoint = process.env.IS_TESTNET ? 'https://node.testnet.like.co/rpc/' : 'https://mainnet-node.like.co/rpc/';
+const denom = process.env.IS_TESTNET ? "nanoekil" : "nanolike"
+const client = await SigningStargateClient.connectWithSigner(
+	rpcEndpoint,
+	wallet,
+);
+
+function sleep(ms) {
+	return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function readCsv(path) {
+	const csv = fs.readFileSync(path);
+	const data = await neatCsv(csv);
+	return data;
+}
+
+async function run() {
+	const data = await readCsv('list.csv');
+	let total = 0
+	const msgArray = data.map((e) => {
+		total += Number(e.LIKE)
+		return MsgSend.fromPartial({
+			fromAddress: firstAccount.address,
+			toAddress: e.address,
+			amount: [{
+				denom: denom,
+				amount: `${new BigNumber(e.LIKE).shiftedBy(9).toFixed(0)}`,
+			}],
+		});
+	})
+	const msgAnyArray = msgArray.map((e) => {
+		return {
+			typeUrl: "/cosmos.bank.v1beta1.MsgSend",
+			value: e,
+		}
+	})
+
+	const fee = {
+		amount: [
+			{
+				denom: denom,
+				amount: `${new BigNumber(msgAnyArray.length).shiftedBy(6).toFixed(0)}`,
+			},
+		],
+		gas: `${new BigNumber(msgAnyArray.length).shiftedBy(5).toFixed(0)}`,
+	};
+
+	console.log('total:', total)
+	console.log('Terminate if not expected (control+c)')
+	await sleep(waitTime)
+
+	const result = await client.signAndBroadcast(
+		firstAccount.address,
+		msgAnyArray,
+		fee,
+		memo
+	);
+	console.log(result)
+}
+run()
