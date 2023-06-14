657424fe8759de8ed53db312201bb1478d451e61
William Chong <me@williamchong.cloud>
2023-06-14 18:24:38 +0800

    [web-ui] 🚸 Add iscn-browser link to index

--- web-ui/pages/index.vue
+++ web-ui/pages/index.vue
@@ -15,5 +15,17 @@
         List and Sell NFT
       </NuxtLink>
     </li>
+    <li>
+      <a :href="iscnBrowserURL" target="_blank">
+        ISCN Browser & Editor
+      </a>
+    </li>
   </ul>
 </template>
+
+<script setup>
+import { ISCN_BROWSER_URL } from '~/constant'
+
+const iscnBrowserURL = ISCN_BROWSER_URL
+
+</script>
