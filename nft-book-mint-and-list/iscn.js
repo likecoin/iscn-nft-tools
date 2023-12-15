255511d9ea5eaf79d90af8a2d1bcdeb128cfc2de
Wei-Jun Hung <weijunhung@outlook.com>
2023-12-16 00:58:55 +0800

    ✨ Use new serial of classId

--- nft-book-mint-and-list/iscn.js
+++ nft-book-mint-and-list/iscn.js
@@ -82,6 +82,13 @@ export async function getSequence() {
   return sequence;
 }
 
+export async function getExistingClassCount(iscnPrefix) {
+  const iscnQueryClient = await getISCNQueryClient();
+  const cosmosQueryClient = await iscnQueryClient.getQueryClient();
+  const { classes } = await cosmosQueryClient.likenft.classesByISCN(iscnPrefix)
+  return classes.length;
+}
+
 export function validateISCNPrefix(input) {
   const res = /^(iscn:\/\/likecoin-chain\/[A-Za-z0-9-_]+)(?:\/([0-9]*))?$/.exec(input);
   if (!res) throw new Error(`Invalid ISCN prefix ${input}, please check the input`);
